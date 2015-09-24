514b0fb9c8f83c0e45302b6ab8bfd444449af084
sidvangala <siddardhavangala@gmail.com>
2015-09-24 14:44:08 -0600

    Deleting

--- main.js
+++ main.js
@@ -29,15 +29,52 @@ setInterval(function(){
 },1000/FPS);
 
 var textX=500,textY = 50;
+
 function Update(){
     textX+=1;
 	//textY+=1;
 	
 }
+var xv = 0;
+var yv = 0;
+var xpos = 200;
+var ypos = 200;
+
+var keys = [];
+
+var keyPressed = function() { 
+  keys[keyCode] = true;
+};
+ 
+var keyReleased = function() { 
+  keys[keyCode] = false; 
+};
+
 function Draw(){
 	context.clearRect(0,0,WIDTH,HEIGHT);
-	context.fillStyle = "#500";
-	context.fillText(""+textX+" "+textY,textX,textY);
+	//context.fillStyle = "#500";
+	//context.fillText(""+textX+" "+textY,textX,textY);
+	 background(0, 0, 0);
+	fill(255, 255, 255); //fill white
+    ellipse(xpos, ypos, 30, 30); //moves the circle
+    if (keyIsPressed && keys[UP]) { //this checks if up arrow is pressed
+        yv = yv - 1;
+    }
+    if (keyIsPressed && keys[DOWN]) {
+        yv = yv + 1;
+    }
+    if (keyIsPressed && keys[LEFT]) {
+        xv = xv - 1;
+    }
+    if (keyIsPressed && keys[RIGHT]) {
+        xv = xv + 1;
+    }
+    xv = xv * 0.9; //slowing it down
+    yv = yv * 0.9;
+    xpos += xv; //setting the positions to the positions + movement
+    ypos += yv;
+	
+	
 }
 
 function DebugLog(debugtext){
